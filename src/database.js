98509089fc5ccfaf63e8515af952081725f13ee3
CARLOS-Datablitz <carlos@datablitz.com.pe>
2025-01-04 12:11:24 -0500

    ec2 local 2

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 
 (async () => {
   try {
-    const db = await connect("mongodb://52.87.255.24:27017/crud-mongo");
+    const db = await connect("mongodb://localhost:27017/crud-mongo");
     console.log("DB connected to", db.connection.name);
   } catch (error) {
     console.error(error);
